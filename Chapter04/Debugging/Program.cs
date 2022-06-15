6fdaf09327160afbfa9d74aadb5c9bddeaab65d8
Artyom Zuykov <zujkov.ks52@gmail.com>
2022-06-15 17:19:33 +0300

    The Debugging directory has been added to the chapter04

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Debugging
+{
+    class Program
+    {
+        /// <summary>
+        /// Возвращает произведение чисел а и b
+        /// </summary>
+        /// <param name="a"> параметр а</param>
+        /// <param name="b"> параметр b</param>
+        /// <returns>число типа double</returns>
+        static double Add(double a, double b)
+        {
+            return a * b;
+        }
+        static void Main()
+        {
+            double a = 4.5;
+            double b = 2.5;
+            double answer = Add(a, b);
+            System.Console.WriteLine($"{a} * {b} = {answer}");
+            Console.ReadLine();
+        }
+    }
+}

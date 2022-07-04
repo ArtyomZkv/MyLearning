bd96d123d1bbf623e2f9235c93eb7af69fa3d6cf
Artyom Zuykov <zujkov.ks52@gmail.com>
2022-07-04 15:40:35 +0300

    PacktLibrary has been added to Chapter05 project

---
+++ Chapter05/PacktLibrary/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Packt.Shared
+{
+    public class Person : object
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+        public WondersOfTheAncientWorld FavoriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+        public List<Person> Children = new ();
+
+        public const string Species = "Homo Sapien";
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+    }
+}

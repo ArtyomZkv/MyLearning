4dd309c6ca94a7981f929657880ec4604f25feaf
Artyom Zuykov <zujkov.ks52@gmail.com>
2022-09-20 21:43:24 +0300

    Chapter07 has been started

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace AssembliesAndNamespaces
+{
+    class Propgram
+    {
+        static void Main()
+        {
+            var doc = new XDocument();
+
+            string s1 = "Hello";
+            string s2 = "World";
+
+            Console.WriteLine($"{s1} {s2}");
+        }
+    }
+}

4c49dd6ee955e427d86a503a347f7d905ac65299
Artyom Zuykov <zujkov.ks52@gmail.com>
2022-10-18 21:59:44 +0300

    fix directory

---
+++ Chapter07/SharedLibrary/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SharedLibrary
+{
+    public class StringExtensions
+    {
+        public static bool IsValidXmlTag(this string input)
+        {
+            return Regex.IsMatch(input, @"^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)$");
+        }
+
+        public static bool IsValidPassword(this string input)
+        {
+            //eight available characters is max
+            return Regex.IsMatch(input, "^[a-zA-Z0-9_-]{8,}$");
+        }
+
+        public static bool IsValidHex(this string input)
+        {
+            //three or six available charecters of hexadecimal number
+            return Regex.IsMatch(input, "^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$");
+        }
+    }
+}

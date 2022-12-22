4e5e2adb0882f22cccc281b662f456e664b8424e
Artyom <zujkov.ks52@gmail.com>
2022-12-22 16:56:24 +0300

    Showing how to summ complex numbers

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -11,6 +11,14 @@ namespace WorkWithNubmers
 
             var atomsInTheUniverse = BigInteger.Parse("123456789012345678901234567890");
             System.Console.WriteLine($"{atomsInTheUniverse, 40:N0}");
+
+            var c1 = new Complex(4, 2);
+            var c2 = new Complex(3, 7);
+            var c3 = c1 + c2;
+
+            System.Console.WriteLine($"{c1} added to {c2} is {c3}");
+
+            
         }
     }
 }

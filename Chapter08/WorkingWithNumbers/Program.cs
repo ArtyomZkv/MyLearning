b86d1128872eb350700843a41713d9d659375e23
Artyom <zujkov.ks52@gmail.com>
2022-12-22 16:49:22 +0300

    showing how many atoms in our Universe

---
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace WorkWithNubmers
+{
+    class Propgram
+    {
+         static void Main()
+         {
+            var largest = ulong.MaxValue;
+            System.Console.WriteLine($"{largest, 40:N0}");
+
+            var atomsInTheUniverse = BigInteger.Parse("123456789012345678901234567890");
+            System.Console.WriteLine($"{atomsInTheUniverse, 40:N0}");
+        }
+    }
+}
+
